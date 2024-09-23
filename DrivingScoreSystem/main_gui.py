3b08be8dfdb101b4ba0adba7625cd94f0de51e68
kjy <rlawndus4801@naver.com>
2024-09-23 10:23:53 +0900

    fix image preview

--- DrivingScoreSystem/main_gui.py
+++ DrivingScoreSystem/main_gui.py
@@ -12,7 +12,6 @@ import struct
 import pickle
 import datetime
 import json
-import ast
 
 from inference import Inference
 from judge import Judge
@@ -27,7 +26,6 @@ TODO
 - 로그 검색기능 완성하기
 - sql 파일 만들기
     - sql 테이블 생성하는 코드 작성하기. 
-- 트랙이랑 통신 뚫기
 - 활성화 되어있는 탭만 작동하게 하기
 - LCD 색 바뀌는거 작동 확인 하기
 - 
@@ -107,6 +105,7 @@ class WindowClass(QMainWindow, from_class):
 
         # 점수 lcd 설정
         self.palette = self.LCD_score.palette()
+        self.palette.setColor(QPalette.WindowText, QColor(0, 255, 0))
 
         # 점수 차감
         self.judge = Judge()
@@ -123,18 +122,24 @@ class WindowClass(QMainWindow, from_class):
         self.cursor.close()
         self.conn.close()
 
+
     def show_admin_tab(self):
         self.Controll.insertTab(self.admin_tab_index, self.admin_tab_widget, "Object_Log")
 
     def hide_admin_tab(self):
         self.Controll.removeTab(self.admin_tab_index)
     
-    # def on_tab_change(self, index):
-    #     if index == 0:
+    def on_tab_change(self, index):
+        if index == 0:
+            # QSocketNotifier 설정 (읽기 가능할 때 알림 받음)
+            self.socket_notifier = QSocketNotifier(self.client_socket.fileno(), QSocketNotifier.Read)
+            self.socket_notifier.activated.connect(self.read_data)
 
+            self.socket_notifier_2 = QSocketNotifier(self.client_socket_2.fileno(), QSocketNotifier.Read)
+            self.socket_notifier_2.activated.connect(self.read_data_2)
 
     def socket_configuration(self, timeout=1):
-        self.host = '172.30.1.33'
+        self.host = '192.168.0.16'
         self.port = 9999
 
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -156,7 +161,7 @@ class WindowClass(QMainWindow, from_class):
         # print(f"Listening on {self.host}:{self.port}...")
     
     def socket_configuration_sectionSpeedReader(self, timeout=1):
-        self.host_2 = '172.30.1.76'
+        self.host_2 = '192.168.0.16'
         self.port_2 = 3333
 
         self.client_socket_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -265,7 +270,7 @@ class WindowClass(QMainWindow, from_class):
             self.label.setPixmap(self.pixmap_monitor)
 
             # 점수 차감
-            self.charge_id, self.penalty, detected_classes, self.is_new_object, self.new_object = self.judge.verdict(detects, cls_set, self.velocity, frame)
+            self.charge_id, self.penalty, detected_classes, self.is_new_object, self.new_object = self.judge.verdict(detects, cls_set, self.velocity, frame, self.section_speed)
             self.update_label(detected_classes)
             
             if self.penalty:
@@ -282,65 +287,42 @@ class WindowClass(QMainWindow, from_class):
             # 새로운 객체 감지 시 DB 업로드
             if self.is_new_object:
                 for e in self.new_object:
-                    print()
-                    print("new object detected!!!!!!!")
-                    print()
                     object_id = self.object_data[e]
                     self.upload_new_object_data(object_id, _json)
                     cv2.imwrite(self.path_admin+self.file_name, frame)
 
         except Exception as e:
             print(f"Error in show_frame: {e}")
-    
-    def upload_penalty_data(self):
-        insert = f"insert into PenaltyLog values ({self.now}, {self.user_id}, {self.charge_id}, {self.velocity})"
-        self.cursor.execute(insert)
-        self.conn.commit()
-    
-    def upload_new_object_data(self, object_id, _json):
-        # json 파일 처리
-        # list[list[dict[]]]
-        # json_new = []
-        # for json_list in _json:
-        #     json_indi_new = []
-        #     for dict in json_list:
-        #         json_indi_new.append({key: dict[key] for key in ['name', 'class', 'confidence', 'box']})
-        #     json_new.append(json_indi_new)
-
-        print("type(_json):", type(_json))
-        print("_json:", _json)
-        print()
-
-        json_new = []
-        for json_list in _json:
-            print("type(json_list):", type(json_list))
-            print("json_list:", json_list) 
-            print()
-            for _dict in json_list:
-                print("type(_dict):", type(_dict))
-                print("_dict:", _dict)
-                print()
-                json_new.append({key: _dict[key] for key in ['name', 'class', 'confidence', 'box']})
-            
-        print("json_new:", json_new)
-        print()
-
-        # json_new = json.dumps(json_new)
-
-        # insert = f"insert into ObjectLog values ('{self.now}', '{_object}', '{self.car_number}', '{self.path_admin}', '{json_new}', '{self.file_name}')"
-        insert = f'''insert into ObjectLog values ('{self.now}', {self.user_id}, {object_id}, '{self.path_admin}', """{json_new}""", '{self.file_name}')'''
-        print("insert:", insert)
-        print()
-        self.cursor.execute(insert)
-        self.conn.commit()
 
     def update_label(self, detected_classes):
         # set 데이터를 문자열로 변환하여 표시 (콤마로 구분)
         label_text = ",\n".join(detected_classes)
         self.label_status_desc.setText(label_text)
     
+    def upload_penalty_data(self, _json):
+        json_new = []
+        for json_list in _json:
+            for _dict in json_list:
+                json_new.append({key: _dict[key] for key in ['name', 'class', 'confidence', 'box']})
+        
+        json_new = json.dumps(json_new)
+        insert = f'''insert into PenaltyLog values ('{self.now}', {self.user_id}, {self.charge_id}, {self.velocity}, '{self.file_name}', '{self.path_user}', '{json_new}', {self.score})'''
+        self.cursor.execute(insert)
+        self.conn.commit()
+    
+    def upload_new_object_data(self, object_id, _json):
+        json_new = []
+        for json_list in _json:
+            for _dict in json_list:
+                json_new.append({key: _dict[key] for key in ['name', 'class', 'confidence', 'box']})
+        
+        json_new = json.dumps(json_new)
+        insert = f'''insert into ObjectLog values ('{self.now}', {self.user_id}, {object_id}, '{self.path_admin}', '{json_new}', '{self.file_name}')'''
+        self.cursor.execute(insert)
+        self.conn.commit()
+    
     def load_user_db(self):
-        query = f"select pl.time, pl.user_id, pl.speed, pd.penalty_type, pd.penalty_score, \
+        query = f"select pl.time, pl.speed, pd.penalty_type, pd.penalty_score, \
                     pl.score, pl.image_path, pl.image_name, pl.json_data \
                     from PenaltyLog pl, PenaltyData pd \
                     where (pl.user_id = {self.user_id}) and (pl.penalty_id = pd.id);"
@@ -351,7 +333,7 @@ class WindowClass(QMainWindow, from_class):
 
         for i, result in enumerate(results):
             
-            date_time, user_id, speed, penalty_type, penalty_score, score, image_path, image_name, json_data = result
+            date_time, speed, penalty_type, penalty_score, score, image_path, image_name, json_data = result
             date_time_str = date_time.strftime("%Y-%m-%d %H:%M")
             self.tableWidget_1.setItem(i, 0, QTableWidgetItem(date_time_str))
             self.tableWidget_1.setItem(i, 1, QTableWidgetItem(self.car_number))
@@ -386,6 +368,7 @@ class WindowClass(QMainWindow, from_class):
         image_path = self.tableWidget_1.item(row, 6).text()
         image_name = self.tableWidget_1.item(row, 7).text()
         json_data = json.loads(self.tableWidget_1.item(row, 8).text())
+        print("json_data:", json_data)
 
         # 이미지에 박스 그리고 보여주기
         image = cv2.imread(image_path+image_name)
@@ -398,14 +381,9 @@ class WindowClass(QMainWindow, from_class):
             y1 = int(y1)
             y2 = int(y2)
 
-            # 바운딩 박스 그리기
             cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-            # 점수 텍스트 표시
             label = f'{conf:.2f}'
             cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 2)
-
-            # 클래스 표시
             cv2.putText(image, cls, (x1 + 40, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 2)
         
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -418,22 +396,10 @@ class WindowClass(QMainWindow, from_class):
     def table2_dclicked(self, row, col):
         image_path = self.tableWidget_2.item(row, 3).text()
         image_name = self.tableWidget_2.item(row, 4).text()
-        # print()
-        # print(f'''{self.tableWidget_2.item(row, 5).text().replace("'", '"')}''')
-        # print()
-        json_data = json.loads(self.tableWidget_2.item(row, 5).text().strip())
-        # json_data = json.loads(f'''{self.tableWidget_2.item(row, 5).text().replace("'", '"')}''')
-        # json_data_raw = self.tableWidget_2.item(row, 5).text()
-        # print("type(json_data_raw):", type(json_data_raw))
-        # print("json_data_raw:", json_data_raw)
-        # json_data = ast.literal_eval(json_data_raw)
-        print("type(json_data):", type(json_data))
-        print("json_data:", json_data)
+        json_data = json.loads(self.tableWidget_2.item(row, 5).text())
 
         image = cv2.imread(image_path+image_name)
         for data in json_data:
-            print("type(data):", type(data))
-            print("data:", data)
             cls = data['name']
             conf = data['confidence']
             x1, x2, y1, y2 = data['box'].values()
@@ -443,10 +409,8 @@ class WindowClass(QMainWindow, from_class):
             y2 = int(y2)
 
             cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
             label = f'{conf:.2f}'
             cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 2)
-
             cv2.putText(image, cls, (x1 + 40, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 2)
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
